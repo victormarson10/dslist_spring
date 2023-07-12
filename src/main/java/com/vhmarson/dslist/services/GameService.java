ff49c93098856695c5e761067517aa7105281054
victor10 <victormarson10@gmail.com>
2023-07-12 16:00:03 -0300

    GET game by id

--- src/main/java/com/vhmarson/dslist/services/GameService.java
+++ src/main/java/com/vhmarson/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.vhmarson.dslist.dto.GameDTO;
 import com.vhmarson.dslist.dto.GameMinDTO;
 import com.vhmarson.dslist.entities.Game;
 import com.vhmarson.dslist.repositories.GameRepository;
@@ -15,6 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long Id) {
+		Game result = gameRepository.findById(Id).get();
+		//Poderia ser implementado a tratativa quando o Id não existir
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
